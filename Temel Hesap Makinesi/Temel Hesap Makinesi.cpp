7c2109182cda518889e5a2638372f50e2c6cd6a7
55mub <55mub@MBERRA>
2026-04-03 14:07:05 +0300

    Proje dosyası ekle.

---
+++ Temel Hesap Makinesi/Temel Hesap Makinesi.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	double num1, num2;
+
+	double sonuc;
+
+	char Operator;
+	cout << "Hesap Makinesine Hosgeldiniz!" << endl;
+
+	cout << "Birinci sayiyi girin: ";
+	cin >> num1;
+;
+
+	cout << "Ikinci sayiyi girin: ";
+	cin >> num2;
+
+	cout << "Operatoru giriniz:" << endl;
+	cin >> Operator;
+
+	switch (Operator)
+	{
+	case '+':
+		sonuc = num1 + num2;
+		break;
+	case '-':
+		sonuc = num1 - num2;
+		break;
+	case '*':
+		sonuc = num1 * num2;
+		break;
+	case '/':
+		if (num2 == 0) {
+			cout << "Sifira bolemezsiniz." << endl;
+			return 0;
+		}
+		else
+			sonuc = num1 / num2;
+		break;
+	default:
+		cout << "Gecersiz Operator girdiniz." << endl;
+		return 0;
+
+	}
+	cout << "Sonuc: " << sonuc;
+
+
+	return 0;
+}
